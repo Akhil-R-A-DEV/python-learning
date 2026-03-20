fe7068025b3f1f9b3930fa78bd6f7d8065dad777
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-21 04:16:18 +0530

    type casting and input

---
+++ 04_TYPE_CASTING/01_typeCasting.py
@@ -0,0 +1,6 @@
+a = 10
+b = "10"
+c = int(b)
+print("type of a : ",type(a))
+print("type of b : ",type(b))
+print("type of c : ",type(c))
---
+++ 05_USER_INPUT/01_input.py
@@ -0,0 +1,4 @@
+a = input("enter a number : ")
+a = int(a)
+b = (a+3)
+print (b)

ac1fea85edb975ea3a63d90026d170f22010c1e5
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-22 00:44:26 +0530

    if elif  else

---
+++ 08_CONDITIONALS/03_if_elif_else.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age : "))
+
+if(age > 18):
+    print("you can drive")
+elif(age == 18):
+    print("you need to take an interview ")
+else:
+    print("you can't drive")

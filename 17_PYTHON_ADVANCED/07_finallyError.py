15e93770d40bf13d7fa5d51de0f05c1b31d5a30d
Akhil R Anand <197573077+Akhil-R-A-DEV@users.noreply.github.com>
2026-03-28 01:42:31 +0530

    finally error

---
+++ 17_PYTHON_ADVANCED/07_finallyError.py
@@ -0,0 +1,12 @@
+a = int(input("Enter 1st num : "))
+b = int(input("Enter 2nd num : "))
+
+
+try:
+    c = a/b
+except Exception as e:
+    print("something went wrong",e)
+else :
+    print("all good")
+finally:
+    print("end")            
